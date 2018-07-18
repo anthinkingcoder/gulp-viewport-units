84b67e7f8e277dd91bacf06380215301eb8d83f3
anthinkingcoder <zhoul@3vjia.com>
2018-07-18 16:38:09 +0800

    add example sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const viewportUnits = require('./index.js')
 const del = require('del');
+const sass = require('gulp-sass');
 
 gulp.task('clean', function (cb) {
     return del(['example/dist'], cb);
@@ -9,5 +10,13 @@ gulp.task('clean', function (cb) {
 gulp.task('css', ['clean'], function (cb) {
     gulp.src('example/**/*.css')
         .pipe(viewportUnits({'onlyCalc': false,selectorBlackList:['.notSelector']}))
+        .pipe(gulp.dest('example/dist'));
+});
+
+
+gulp.task('sass', ['clean'], function (cb) {
+    gulp.src('example/**/*.scss')
+        .pipe(sass())
+        .pipe(viewportUnits({selectorBlackList:['.notSelector','.demo']}))
         .pipe(gulp.dest('example/dist'));
 });
